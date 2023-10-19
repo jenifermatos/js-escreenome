82c2d409dfa338898a1724d75a3ab44de8c56b8c
jenifermatos <145689627+jenifermatos@users.noreply.github.com>
2023-10-19 11:24:16 -0300

    atualizando js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+  background("white")
+}
+
+function draw()  {
+  stroke("blue")
+  fill("red");
   
-  function draw() {
-    background(220);
-  }
+  if(mouseIsPressed) {
+rect(mouseX,mouseY, 20, 20);
+  
+}
+}
